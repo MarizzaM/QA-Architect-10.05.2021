63c39f65d27eb429d945476eda46878dccd4005f
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-05-11 12:17:59 +0300

    Update CustomerController.java

--- customer/CustomerController.java
+++ customer/CustomerController.java
@@ -3,8 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+
 @RestController
 public class CustomerController {
     static ArrayList<Customer> customers = new ArrayList<>();
@@ -15,46 +14,28 @@ public class CustomerController {
         return customerDAO.getAllCustomers();
     }
 
-
     @GetMapping("/customer/{id}")
     public Customer doGetCustomerById(@PathVariable("id") int id)
     {
         return customerDAO.getCustomerById(id);
     }
-//
-//    @PostMapping("/customer")
-//    public String addCustomer(@RequestBody Customer c)
-//    {
-//        m_customers.add(c);
-//        return "Customer add";
-//    }
-//
-//    @PutMapping("/customer/{id}")
-//    public String doUpdateCustomerByID(@PathVariable("id") int id,
-//                                     @RequestBody Customer sent)
-//    {
-//        for (Customer c : m_customers) {
-//            if (c.getId() == id){
-//                c.setId(sent.getId());
-//                c.setName(sent.getName());
-//                c.setAddress(sent.getAddress());
-//                return  "Customer updated";
-//            }
-//        }
-//        return "Customer not found";
-//    }
-//
-//    @DeleteMapping ("/customer/{id}")
-//    public String deleteCustomerById(@PathVariable("id") int id)
-//    {
-//        Iterator<Customer> it = m_customers.iterator();
-//        while(it.hasNext()) {
-//            Customer i = it.next();
-//            if(i.getId() == id) {
-//                it.remove();
-//                return "Customer deleted";
-//            }
-//        }
-//        return "Customer not found";
-//    }
+
+    @PostMapping("/customer")
+    public void addCustomer(@RequestBody Customer c)
+    {
+        customerDAO.insertCustomer(c);
+    }
+
+    @PutMapping("/customer/{id}")
+    public void doUpdateCustomerByID(@PathVariable("id") int id,
+                                     @RequestBody Customer sent)
+    {
+        customerDAO.updateCustomer(sent, id);
+    }
+
+    @DeleteMapping ("/customer/{id}")
+    public void deleteCustomerById(@PathVariable("id") int id)
+    {
+        customerDAO.deleteCustomer(id);
+    }
 }
